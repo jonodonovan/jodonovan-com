08728f4fb09d88b7972047a62e9dbae4d76fe629
Jon ODonovan <jonodonovan@users.noreply.github.com>
2021-02-26 14:45:00 -0500

    Removed form

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -41,53 +41,6 @@
                 <a href="/about">About</a> | <a href="/development">Development</a> | <a href="/community">Community</a></p>
             </div>
         </div>
-        <br>
-        <div class="col-md-4">
-            <div class="jumbotron" style="background-color:#2196F3;padding:20px;box-shadow: 0 4px 6px rgba(50,50,93,.11), 0 1px 3px rgba(0,0,0,.08);">
-                <h3 style="color:white;font-size:38px;text-align:center;margin:10px 0px;">Let's get in touch!</h3>
-                <form method="POST" action="{{route('contact.submit')}}">
-                {{csrf_field()}}
-                    <div class="form-group">
-                        <input type="text" class="form-control" name="name" id="name" placeholder="Name" value="{{old('name')}}" required>
-                        @if ($errors->has('name'))
-                            <span class="help-block">
-                                <strong style="color:white;">{{ $errors->first('name') }}</strong>
-                            </span>
-                        @endif
-                    </div>
-                    <div class="form-group">
-                        <input type="text" class="form-control" name="companyname" id="companyname" placeholder="Company Name" value="{{old('companyname')}}">
-                        @if ($errors->has('companyname'))
-                            <span class="help-block">
-                                <strong style="color:white;">{{ $errors->first('companyname') }}</strong>
-                            </span>
-                        @endif
-                    </div>
-                    <div class="form-group">
-                        <input type="email" class="form-control" name="email" id="email" placeholder="Email" value="{{old('email')}}" required>
-                        @if ($errors->has('email'))
-                            <span class="help-block">
-                                <strong style="color:white;">{{ $errors->first('email') }}</strong>
-                            </span>
-                        @endif
-                    </div>
-                    <div class="form-group">
-                        <input type="text" class="form-control" name="phonenumber" id="phonenumber" placeholder="Phone Number" value="{{old('phonenumber')}}">
-                    </div>
-                    <div class="form-group">
-                        <label for="message" class="control-label" style="color:white;">How can I help you?</label>
-                        <textarea style="resize:none;" rows="5" id="message" class="form-control" name="message"  required>{{old('message')}}</textarea>
-                        @if ($errors->has('message'))
-                            <span class="help-block">
-                                <strong style="color:white;">{{ $errors->first('message') }}</strong>
-                            </span>
-                        @endif
-                    </div>
-                    @captcha
-                    <button type="submit" class="btn btn-main btn-lg" style="background-color:white;">Send</button>
-                </form>
-            </div>
-        </div>
     </div>
 </div>
 @endsection
